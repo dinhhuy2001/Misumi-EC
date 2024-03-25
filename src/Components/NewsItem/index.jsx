9d0aed446ff0c828149e4b761175a83511151934
dinhhuy <tdhuy1410@gmail.com>
2024-03-25 14:09:10 +0900

    update WatchedHistory && update old version

--- src/Components/NewsItem/index.jsx
+++ src/Components/NewsItem/index.jsx
@@ -4,13 +4,13 @@ const NewsItem = ({ src, title, time, linkContent, alt, newNews }) => {
     return (
         <li className="news__item">
             <dl>
-                <dt className="inline-flex justify-center items-center">
+                <dt className="inline-flex justify-center items-center news__img__container">
                     <div className="news__img">
                         <img src={src} alt={alt} />
                     </div>
                     <div className="news__title text-center bold">{title}</div>
                 </dt>
-                <dd>
+                <dd style={{ fontSize: '12px' }}>
                     <div className="flex gap-2 relative">
                         <span className="news__time">{time}</span>
                         {newNews && <strong className="news--new flex items-center">NEW</strong>}
